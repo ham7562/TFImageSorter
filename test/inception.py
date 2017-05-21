eb23cd836991ebefaab45d21511e11923fcf90e7
ham7562 <gkaduddlf2@gmail.com>
2017-05-22 00:30:26 +0900

    스타트

---
+++ test/inception.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os.path
+import re
+import sys
+import tarfile
+
+import numpy as np
+import tensorflow as tf
+from six.moves import urllib
+
+import 
