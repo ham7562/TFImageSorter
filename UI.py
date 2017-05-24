294b63fbf8f3138f85751be15d6e3126004aa94b
ham7562 <gkaduddlf2@gmail.com>
2017-05-24 22:07:06 +0900

    sample UI 완성
    경로 누르면 꺼지는 버그 수정

--- UI.py
+++ UI.py
@@ -1,54 +1,49 @@
+# -*- coding: utf-8 -*-
 
-import sys
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
-      
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800,400 )
+        MainWindow.resize(800, 409)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.startDirT = QtWidgets.QTextEdit(self.centralwidget)
-        self.startDirT.setGeometry(QtCore.QRect(20, 40, 630, 41))
+        self.startDirT.setGeometry(QtCore.QRect(20, 30, 631, 31))
         self.startDirT.setObjectName("startDirT")
-        
-        
         self.startDirB = QtWidgets.QPushButton(self.centralwidget)
-        self.startDirB.setGeometry(QtCore.QRect(660, 40, 120, 40))
+        self.startDirB.setGeometry(QtCore.QRect(660, 30, 121, 31))
         self.startDirB.setObjectName("startDirB")
-        
-        
-        
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(20, 10, 171, 16))
         self.label.setObjectName("label")
-        
         self.endDirT = QtWidgets.QTextEdit(self.centralwidget)
-        self.endDirT.setGeometry(QtCore.QRect(20, 120, 630, 41))
+        self.endDirT.setGeometry(QtCore.QRect(20, 90, 631, 31))
         self.endDirT.setObjectName("endDirT")
-        
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(20, 90, 171, 16))
+        self.label_2.setGeometry(QtCore.QRect(20, 70, 171, 16))
         self.label_2.setObjectName("label_2")
-        
         self.endDirB = QtWidgets.QPushButton(self.centralwidget)
-        self.endDirB.setGeometry(QtCore.QRect(660, 120, 120, 40))
+        self.endDirB.setGeometry(QtCore.QRect(660, 90, 121, 31))
         self.endDirB.setObjectName("endDirB")
-        
         self.startB = QtWidgets.QPushButton(self.centralwidget)
-        self.startB.setGeometry(QtCore.QRect(660, 200, 120, 40))
+        self.startB.setGeometry(QtCore.QRect(660, 150, 121, 191))
         self.startB.setObjectName("startB")
-        
-
-        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(20,190,630,120))
-        self.textBrowser.setObjectName("textBrowser")
-        
+        self.LogTxet = QtWidgets.QTextBrowser(self.centralwidget)
+        self.LogTxet.setGeometry(QtCore.QRect(20, 150, 631, 192))
+        self.LogTxet.setObjectName("LogTxet")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(20, 130, 56, 12))
+        self.label_3.setObjectName("label_3")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -58,22 +53,21 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         
+        self.startDirB.clicked.connect(self.dirBClicked)
         
-        
-        
-
+    def dirBClicked(self):
+       dirtxt = QtWidgets.QFileDialog.getExistingDirectory(None,'Open file', 'c:/')
+       self.startDirT.setText(dirtxt)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "이미지 분류기"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.startDirB.setText(_translate("MainWindow", "경로"))
         self.label.setText(_translate("MainWindow", "분류할 이미지 경로 지정"))
         self.label_2.setText(_translate("MainWindow", "분류될 경로 지정"))
         self.endDirB.setText(_translate("MainWindow", "경로"))
         self.startB.setText(_translate("MainWindow", "분류 시작"))
-        
-    
-        
+        self.label_3.setText(_translate("MainWindow", "로그"))
 
 
 if __name__ == "__main__":
@@ -84,6 +78,4 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
-
 
